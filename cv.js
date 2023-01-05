c2ede843e05931585fa182a2d669fb2fc3c781f7
Jan Motene <motenejo@gmail.com>
2023-01-05 13:04:01 +0200

    Modifying cv.js Files

--- cv.js
+++ cv.js
@@ -19,35 +19,6 @@ const findMyState = () => {
     }
 
     navigator.geolocation.getCurrentPosition(success, error);
-
-    // ***********WEATHER FORECAST*********//
-    // let goPosition = function(pos) {
-    //     let latitude = pos.coords.latitude;
-    //     let longitude = pos.coords.longitude;
-    //     getForecast(latitude, longitude);
-    // }
-
-    // let getForecast = function(latitude, longitude) {
-    //     let weaherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?latitude=${latitude}&longitude=${longitude}&exclude=current,minutelyhourly&appid=a5e46fcc590996443a5ad3396df941df";
-    //     getWeatherText(weaherApiUrl);
-    // }
-
-    // async function getWeatherText(weatherApiUrl) {
-    //     let weatherObject = wait fetch(weatherApiUrl);
-    //     let weatherText = wait weatherObject.text();
-    //     parseWeather(weatherApiUrl);
-    // }
-
-    // let parseWeather = function(weatherText) {
-    //     let weatherJSON = JSON.parse(weatherText);
-    //     let dailyForecast = weatherJSON.day;
-    //         }
-    //         let dayOfWeek = getDayOfWeek(today);
-    //         let description = day.weather[0].description;
-    //         let icon = day.weather[0].icon;
-    //         let sunrise = timestampToTime(day.sunrise);
-    //     }
-    // }
 }
 
 document.querySelector('.find-state').addEventListener('click', findMyState);
